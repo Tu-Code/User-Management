42f5564d0d686bb92e65bce7862727f59667a2d3
Sarah Solarin <solarinsarah3@gmail.com>
2023-01-06 11:00:07 +0000

    Updated server.js to use views and routes

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
+app.use(express.static('views'));
+
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 
 const db = mongoose.connection
@@ -10,6 +13,10 @@ db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to database.'))
 
 app.use(express.json())
+
+const webRoutes = require('./routes/web')
+
+app.use(webRoutes)
 
 app.listen(3000, () => console.log('Server Started.'))
 
