892a4d9562b872ec41483e876447d56ea623bd76
Sarah Solarin <solarinsarah3@gmail.com>
2023-01-06 10:32:05 +0000

    Implemented search and pagination features

--- routes/web.js
+++ routes/web.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userService = require('../services/userService')
 const userValidation = require('../validations/user')
+const { User } = require('../models/User');
 
 router.post('/users', async (req, res) =>{
     const { error } = userValidation.validate(req.body);
@@ -30,6 +31,25 @@ router.post('/users', async (req, res) =>{
 // GET /users?search=john&page=2&limit=10
 router.get('/users', (req, res) => {
     // do something
+    const search = req.query.search || '';
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 20;
+  
+    User.find({
+      $or: [
+        { firstName: { $regex: search, $options: 'i' } },
+        { lastName: { $regex: search, $options: 'i' } },
+        { email: { $regex: search, $options: 'i' } }
+      ]
+    })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .exec((err, users) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        res.json({ users });
+      });
 });
 
 //find a user by ID
