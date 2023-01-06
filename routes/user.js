331bdb3c10548bc0c6887caeecb05fba6a387122
Sarah Solarin <solarinsarah3@gmail.com>
2023-01-06 21:48:19 +0000

    Reversed commits to check for issue

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const express = require('express');
 const { User } = require('../models/User');
 const userService = require('../services/user')
 const userValidation = require('../validations/user');
-const passwordValidation = require('../validations/password')
 const bcrypt = require('bcryptjs');
 const router = express.Router();
 const path = require('path');
@@ -40,21 +38,27 @@ router.post('/users', async (req, res) =>{
 });
 
 // GET /users?search=john&page=2&limit=10
-router.get('/users', async (req, res) => {
+router.get('/users', (req, res) => {
     // do something
     const search = req.query.search || '';
     const page = req.query.page || 1;
     const limit = req.query.limit || 20;
   
-    try {
-        const result = await userService.findAllUsers({search, page, limit});
-
-        res.json({ result });
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("Failed to retrieve users");
-    }
+    User.find({
+      $or: [
+        { firstName: { $regex: search, $options: 'i' } },
+        { lastName: { $regex: search, $options: 'i' } },
+        { email: { $regex: search, $options: 'i' } }
+      ]
+    })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .exec((err, users) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        res.json({ users });
+      });
 });
 
 //find a user by ID
@@ -73,21 +77,58 @@ router.get("/users/:id", async (req, res) => {
     }
 });
 
-router.put('/users/:id/update/password', async (req, res) => {
-    const { error } = passwordValidation.validate(req.body);
-    if (error) return res.status(422).send(error.details[0].message)
+router.put('/users/:id', (req, res) => {
+    User.findOne({ _id: req.params.id }).then(user => {
+        // Check if current password is correct
+        bcrypt.compare(req.body.currentPassword, user.password)
+        .then(isMatch => {
+            if(isMatch) {
+                // If current password is correct, update password
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(req.body.newPassword, salt, (err, hash) => {
+                        user.password = hash;
+                        user.save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                    });
+                });
+            } else {
+                return res.status(400).json({ password: 'Current password is incorrect' });
+            }
+        });
+    });
+});
 
+module.exports = router;
+
+//find a user by ID
+router.get("/users/:id", async (req, res) => {
+    // do something
     try {
-        const user = await userService.updatePssword(req.params.id, req.body.password);
+        const user = await userService.findOneById(req.params.id);
     
         if (!user) {
-          return res.status(400).send("Failed to update user password");
+          return res.status(404).send("User not found");
         }
     
         res.json({ user });
     } catch (err) {
-        res.status(500).send("Failed to update user password");
+        res.status(500).send("Failed to retrieve user");
     }
+});
+
+router.put('/users/:id', (req, res) => {
+   try{
+    const user = userService.changePassword(req.params.id, req.body)
+
+    if (!user) {
+        return res.status(400).send("user doesn't exists");
+    }
+    
+    res.status(201).send(user);
+   } catch (err){
+        res.status(500).send("Failed to retrieve user");
+   }
 });
 
 module.exports = router;
